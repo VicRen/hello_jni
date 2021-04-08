4480953751fc7aa9f7ea70c690748e5a908e4929
VicRen <vic.ren@me.com>
2021-04-08 10:04:12 +0800

    add cross compile

---
+++ cross_compile/mymath.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include"mymath.h"
+int myadd(int a, int b)
+{
+    return a + b;
+}
+ 
+int mysub(int a, int b)
+{
+    return a - b;
+}
+ 
+int mymul(int a, int b)
+{
+    return a * b;
+}
---
+++ cross_compile/mymath.h
@@ -0,0 +1,6 @@
+#ifndef _MYMATH_H
+#define _MYMATH_H
+int myadd(int a, int b);
+int mysub(int a, int b);
+int mymul(int a, int b);
+#endif
